61069999071d9a2c58b5b746b68f87ce6a0b4672
Muskaan Verma <51285206+muskaanv@users.noreply.github.com>
2020-10-21 19:47:28 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,25 @@
+//jshint esverion: 6
+
+exports.GetDate =
+  function() {
+    const today = new Date();
+
+    const options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+    };
+
+    return today.toLocaleDateString("en-US", options);
+  };
+
+exports.GetDay =
+  function() {
+    const today = new Date();
+
+    const options = {
+      weekday: "long"
+    };
+
+    return today.toLocaleDateString("en-US", options);
+  };
